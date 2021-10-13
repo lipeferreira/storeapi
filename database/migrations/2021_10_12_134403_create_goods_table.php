6472c4691ae9281e06a83882ee7aed3f57766008
Felipe Ferreira <felipe.ferreira1988@hotmail.com>
2021-10-12 23:10:50 -0300

    create warehouse migrations

---
+++ database/migrations/2021_10_12_134403_create_goods_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGoodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('goods', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('color_id');
+            $table->foreignId('brand_id');
+            $table->string('name');
+            $table->text('description');
+            $table->string('size');
+            $table->double('price');
+            $table->timestamps();
+            $table->foreign('color_id')->references('id')->on('colors');
+            $table->foreign('brand_id')->references('id')->on('brands');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('goods');
+    }
+}
---
+++ database/migrations/2021_10_12_140408_create_good_photos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGoodPhotosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('good_photos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('good_id');
+            $table->string('photo');
+            $table->timestamps();
+            $table->foreign('good_id')->references('id')->on('goods');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('good_photos');
+    }
+}
